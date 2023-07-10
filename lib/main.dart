dd15c4f867931f26d562a647c2c13d41066deca4
Roberto Notario <roberto.notario@cuisine.digital>
2023-07-10 19:15:58 +0100

    sealed classes and object patterns

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class DocumentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final (title, :modified) = document.metadata;
+    final blocks = document.getBlocks();
 
     return Scaffold(
       appBar: AppBar(
@@ -32,13 +33,60 @@ class DocumentScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Center(
-            child: Text(
-              'Last modified $modified',
+          Text('Last modified: ${formatDate((modified))}'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
             ),
-          )
+          ),
         ],
       ),
     );
   }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    super.key,
+    required this.block,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: switch (block) {
+        HeaderBlock(:final text) =>
+          Text(text, style: Theme.of(context).textTheme.displayMedium),
+        ParagraphBlock(:final text) => Text(text),
+        CheckboxBlock(:final text, :final isChecked) => Row(
+            children: [
+              Checkbox(value: isChecked, onChanged: (_) {}),
+              Text(text, style: Theme.of(context).textTheme.bodySmall)
+            ],
+          )
+      },
+    );
+  }
+}
+
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days, isNegative: true) when days < -7 =>
+      '${(days ~/ 7).abs()} weeks ago',
+    Duration(inDays: final days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now',
+  };
 }
