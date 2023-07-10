9b8aac5a0b0f509c758f5cb1c97760dcd9473383
Roberto Notario <roberto.notario@cuisine.digital>
2023-07-10 18:40:42 +0100

    if-case & refutable pattern

--- lib/data.dart
+++ lib/data.dart
@@ -6,16 +6,40 @@ class Document {
   Document() : _json = jsonDecode(documentJson);
 
   (String, {DateTime modified}) get metadata {
-    if (_json.containsKey('metadata')) {
-      final metadataJson = _json['metadata'];
-      if (metadataJson is Map) {
-        final title = metadataJson['title'] as String;
-        final localModified =
-            DateTime.parse(metadataJson['modified'] as String);
-        return (title, modified: localModified);
-      }
+    if (_json
+        case {
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
     }
-    throw const FormatException('Unexpected JSON');
+  }
+
+  List<Block> getBlocks() {
+    if (_json case {'blocks': List blocksJson}) {
+      return [for (final blockJson in blocksJson) Block.fromJson(blockJson)];
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+class Block {
+  final String type;
+  final String text;
+
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': final type, 'text': final text}) {
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
   }
 }
 
